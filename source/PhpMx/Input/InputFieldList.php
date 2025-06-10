240e19268212bb6dfb6655895438254fd3cec50e
AndreRicardoé <contatoadmx@gmail.com>
2025-06-10 18:57:51 -0300

    ident

--- source/PhpMx/Input/InputFieldList.php
+++ source/PhpMx/Input/InputFieldList.php
@@ -4,9 +4,9 @@ namespace PhpMx\Input;
 
 class InputFieldList extends InputField
 {
-  function __construct(string $name, ?string $alias = null, mixed $value = null)
-  {
-    $value = is_array($value) ? implode(',', $value) : $value;
-    parent::__construct($name, $alias, $value);
-  }
+    function __construct(string $name, ?string $alias = null, mixed $value = null)
+    {
+        $value = is_array($value) ? implode(',', $value) : $value;
+        parent::__construct($name, $alias, $value);
+    }
 }
--- source/PhpMx/Input/InputFieldUpload.php
+++ source/PhpMx/Input/InputFieldUpload.php
@@ -4,15 +4,15 @@ namespace PhpMx\Input;
 
 class InputFieldUpload extends InputField
 {
-  function __construct(string $name, ?string $alias = null, mixed $value = null)
-  {
-    parent::__construct($name, $alias, $value);
+    function __construct(string $name, ?string $alias = null, mixed $value = null)
+    {
+        parent::__construct($name, $alias, $value);
 
-    $this->preventTag(false);
-    $this->scapePrepare(false);
+        $this->preventTag(false);
+        $this->scapePrepare(false);
 
-    $this->validate(fn($v) => is_array($v), '[#name] não foi enviado');
-    $this->validate(fn($v) => $v['error'] != 1 && $v['error'] != 2, 'fileSize');
-    $this->validate(fn($v) => $v['error'] == 0, 'fileError');
-  }
+        $this->validate(fn($v) => is_array($v), '[#name] não foi enviado');
+        $this->validate(fn($v) => $v['error'] != 1 && $v['error'] != 2, 'fileSize');
+        $this->validate(fn($v) => $v['error'] == 0, 'fileError');
+    }
 }
