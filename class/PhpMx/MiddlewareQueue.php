9a07d392947b003301e86ceff57bd53745df7d1c
André Ricardo <contatoadmx@gmail.com>
2025-12-15 02:53:14 -0300

    middlewareQueue

--- class/PhpMx/MiddlewareQueue.php
+++ class/PhpMx/MiddlewareQueue.php
@@ -2,25 +2,24 @@
 
 namespace PhpMx;
 
+use Closure;
 use Exception;
 
 /** Classe responsável pela execução encadeada de middlewares. */
-class Middleware
+class MiddlewareQueue
 {
-    protected static array $QUEUE = [];
+    protected array $QUEUE = [];
+
+    function __construct(protected Request $request, protected Response $response) {}
 
     /** Executa uma fila de middlewares retornando a action */
-    static function run(array $queue, $action)
+    function __invoke(array $queue, Closure $action)
     {
-        if (!is_closure($action))
-            $action = fn() => $action;
-
         $queue[] = $action;
-
         return self::execute($queue);
     }
 
-    protected static function execute(mixed &$queue): mixed
+    protected function execute(mixed &$queue): mixed
     {
         if (count($queue)) {
             $middleware = array_shift($queue);
@@ -32,10 +31,10 @@ class Middleware
         return null;
     }
 
-    protected static function getCallable(mixed $middleware)
+    protected function getCallable(mixed $middleware)
     {
         if (is_array($middleware))
-            return fn($next) => self::run([...$middleware], $next);
+            return fn($next) => $this([...$middleware], $next);
 
         if (is_string($middleware)) {
 
@@ -51,6 +50,14 @@ class Middleware
                 throw new Exception("Middleware [$middleware] not found");
 
             $action = Import::return($actionFile);
+
+            if (!is_object($action))
+                throw new Exception("Middleware [$middleware] not is object");
+
+            if (is_extend($action, Context::class)) {
+                $action->request = $this->request;
+                $action->response = $this->response;
+            }
 
             if (!is_callable($action))
                 throw new Exception("Middleware [$middleware] cannot be called");
